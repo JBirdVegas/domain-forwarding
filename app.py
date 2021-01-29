3c1f16eaa55cc48215927021e4be69df874aff72
Stanford, Jon <jonathan.stanford@asurion.com>
2021-01-29 10:42:40 -0600

    Add wallstreetbetz.com

--- app.py
+++ app.py
@@ -2,10 +2,21 @@
 
 from aws_cdk import core
 
-from forwarding.forwarding_stack import ForwardingStack
+from forwarding.forwarding_stack import ForwardingStack, Config
 
+the_stanford_girls = core.App()
+wall_street_betz = core.App()
 
-app = core.App()
-ForwardingStack(app, "forwarding")
+ForwardingStack(the_stanford_girls, "theStanfordGirlsForwarding", Config(
+    domain_name='thestanfordgirls.com',
+    zone_id='Z0681220185CG6SLGWTT7',
+    redirect_url='https://poshmark.com/closet/hbstanford'
+))
+ForwardingStack(wall_street_betz, "wallstreetbetzForwarding", Config(
+    domain_name='wallstreetbetz.com',
+    zone_id='Z02346722ZLQ8556O63VJ',
+    redirect_url='https://www.reddit.com/r/wallstreetbets/'
+))
 
-app.synth()
+the_stanford_girls.synth()
+wall_street_betz.synth()
--- forwarding/forwarding_stack.py
+++ forwarding/forwarding_stack.py
@@ -1,9 +1,83 @@
-from aws_cdk import core
+import json
+
+from aws_cdk import (core,
+                     aws_apigateway,
+                     aws_certificatemanager as cert_manager,
+                     aws_route53_targets as targets,
+                     aws_route53 as route53)
+
+
+class Config:
+    def __init__(self, domain_name, zone_id, redirect_url):
+        self.domain_name = domain_name
+        self.zone_name = domain_name.split('.')[0]
+        self.zone_id = zone_id
+        self.redirect_url = redirect_url
 
 
 class ForwardingStack(core.Stack):
-
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, cfg: Config, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        self.config = cfg
+        self.hosted_zone = route53.HostedZone.from_hosted_zone_attributes(self, "hostedZoneForApiGateway",
+                                                                          hosted_zone_id=self.config.zone_id,
+                                                                          zone_name=self.config.domain_name)
+        self.cors_options = aws_apigateway.CorsOptions(allow_headers=['*'],
+                                                       allow_methods=['*'],
+                                                       allow_origins=['*'])
+        self.certificate = cert_manager.DnsValidatedCertificate(
+            self,
+            f'domain_cert_for_{self.config.zone_name}',
+            domain_name=self.config.domain_name,
+            hosted_zone=self.hosted_zone,
+            validation_method=cert_manager.ValidationMethod.DNS)
+        domain_name_options = aws_apigateway.DomainNameOptions(certificate=self.certificate,
+                                                               domain_name=self.config.domain_name,
+                                                               security_policy=aws_apigateway.SecurityPolicy.TLS_1_2)
+        self.api = aws_apigateway.RestApi(self, "restapi",
+                                          rest_api_name='rest-forwarder',
+                                          domain_name=domain_name_options,
+                                          deploy=True,
+                                          default_cors_preflight_options=self.cors_options,
+                                          deploy_options=aws_apigateway.StageOptions(cache_data_encrypted=True,
+                                                                                     stage_name='prod'))
+        response = aws_apigateway.IntegrationResponse(
+            status_code="302",
+            response_parameters={
+                'method.response.header.location': f"'{self.config.redirect_url}'"
+            },
+            response_templates={
 
-        # The code that defines your stack goes here
+            })
+        self.api.root.add_method(
+            "ANY",
+            aws_apigateway.MockIntegration(
+                integration_responses=[response],
+                passthrough_behavior=aws_apigateway.PassthroughBehavior.WHEN_NO_MATCH,
+                request_templates={
+                    "application/json": json.dumps({
+                        "statusCode": 302,
+                        "headers": {
+                            "location": self.config.redirect_url
+                        }
+                    })
+                }
+            ),
+            method_responses=[
+                aws_apigateway.MethodResponse(
+                    response_models={
+                        "application/json": aws_apigateway.Model.EMPTY_MODEL
+                    },
+                    status_code="302",
+                    response_parameters={
+                        'method.response.header.location': True
+                    },
+                )
+            ]
+        )
+        # noinspection PyTypeChecker
+        api_domain_target = route53.RecordTarget.from_alias(targets.ApiGatewayDomain(self.api.domain_name))
+        self.arecord = route53.ARecord(self, 'arecord',
+                                       target=api_domain_target,
+                                       zone=self.hosted_zone,
+                                       record_name=self.config.domain_name)
